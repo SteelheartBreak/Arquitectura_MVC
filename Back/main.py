16d24640f4148b4a1691509923ce37c697283100
gams2004 <gabrielmartin2k4@gmail.com>
2024-03-18 11:38:30 -0500

    Fixed bug

--- Back/main.py
+++ Back/main.py
@@ -86,7 +86,7 @@ async def crear_usuario_endpoint(nombre_usuario: str):
         cursor.execute("INSERT INTO usuario (nombre_usuario) VALUES (:nombre_usuario)",
                        {"nombre_usuario": nombre_usuario})
         connection.commit()
-        return {{"message":"Usuario creado correctamente"}}
+        return {"message":"Usuario creado correctamente"}
     except cx_Oracle.Error as error:
         print("Error al crear usuario:", error)
         raise HTTPException(status_code=500, detail="Error al crear usuario en la base de datos")
